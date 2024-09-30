2291bd8e1ddc0b914e63e75c1b996c7c68831466
weamibrahim <weamibrahim46@gmail.com>
2024-10-01 01:44:04 +0300

    make logs

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -159,9 +159,11 @@ UserController.resetPassword = async (req, res) => {
 
  
   const token = req.headers.authorization.split(' ')[1]; 
+  console.log('token:', token);
 
   try {
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    console.log('decodedToken:', decodedToken);
     const userId = decodedToken.userId; 
 
     if (!userId) {
